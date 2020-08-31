89f4d52c57d2659a9c71598a0e13398101834580
corydave <corydave@users.noreply.github.com>
2020-08-31 13:50:47 -0400

    Create HelloWorld.java

---
+++ 01-Intro/HelloWorld.java
@@ -0,0 +1,13 @@
+// Dave Ghidiu
+// 2020-08-31
+// Will output "Hello World" on the screen.
+
+public class HelloWorld {
+    
+  public static void main (String[] args) {
+
+    System.out.println("Hello, World!");
+  
+  } 
+  
+}
