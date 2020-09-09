66e83ac486568ea897a2dd10dbf4c03e288f441d
corydave <corydave@users.noreply.github.com>
2020-09-09 12:57:07 -0400

    Create Initials.java

---
+++ 01-Intro/Initials.java
@@ -0,0 +1,47 @@
+// NAME: Dave Ghidiu
+// DATE: 2020-09-02
+// DESC: Output my initials
+
+/*
+    TODO
+     - Whitespace
+     - Comments
+     - Output my initials
+ */
+
+public class Initials {
+    
+    public static void main(String[] args) {
+
+      // ========== INSERT YOUR CODE HERE ==========
+    
+        System.out.println("DDDDD       GGGGG    ");    
+        System.out.println("D    D     G     G   ");
+        System.out.println("D    D    G          ");
+        System.out.println("D    D    G     GG   ");
+        System.out.println("D    D     G     G   ");
+        System.out.println("DDDDD       GGGGG    ");
+        
+        // System.out.println();
+        // System.out.println();
+        // System.out.println("GAME OVER MAN!");
+        
+
+    }
+}
+
+// =============== LAB SUMMARY ===============
+
+/*
+
+1. What was the hardest part of this lab?
+
+
+
+
+2. What will you always remember (never forget) from this exercise?
+
+
+
+
+*/
