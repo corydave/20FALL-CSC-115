077402fde2ba33cdde4195d801b0358a13ea06cb
corydave <corydave@users.noreply.github.com>
2020-12-16 10:39:21 -0500

    Create FinalDiscussion.java

---
+++ 11-FINAL/FinalDiscussion.java
@@ -0,0 +1,22 @@
+public class FinalDiscussion {
+
+  public static void main (String[] args) {
+
+
+        try {
+
+            System.out.print("Loading");
+            Thread.sleep(1000);
+            System.out.print(".");
+            Thread.sleep(1000);
+            System.out.print(".");
+            Thread.sleep(1000);
+            System.out.print(".");
+
+        } catch (Exception e) {
+
+        }
+
+  }
+
+}
