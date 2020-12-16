511ce47474aa040e80a45c6ed955aff686ddd964
corydave <corydave@users.noreply.github.com>
2020-12-16 11:00:56 -0500

    Update FinalDiscussion.java

--- 11-FINAL/FinalDiscussion.java
+++ 11-FINAL/FinalDiscussion.java
@@ -18,5 +18,24 @@ public class FinalDiscussion {
         }
 
   }
+  
+    public static boolean checkDice(int[] die) {
+
+        // This method will walk through the array, and if any two
+        // consecutive elements are not equal, this method will
+        // return FALSE. But if all consecutive elements are equal,
+        // return TRUE.
+        for (int i = 1; i < die.length; i++) {
+
+            if (die[i] != die[i-1]) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+  
 
 }
